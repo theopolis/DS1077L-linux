889ceda63258bc9513a76c221119d3e1bacfcfd7
Teddy <teddy@prosauce.org>
2013-02-08 02:38:39 -0700

    Added README and GNU note.

--- set_osc.c
+++ set_osc.c
@@ -1,3 +1,21 @@
+/*
+ * set_osc.c
+ * Author: Teddy Reed <teddy@prosauce.org>
+ *
+ * Short and simple way to set P/N on a DS1077L OSC.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ */
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
